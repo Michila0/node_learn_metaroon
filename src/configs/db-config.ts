73f49aa9c3ec45b5ddea46e043f412e5f810c0ff
Michila0 <michila.bimsara12@gmail.com>
2024-02-04 16:52:00 +0530

    chore: copy paste utils page

---
+++ src/configs/db-config.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export abstract class DbConfig {
+    public static DB_HOST: string = process.env.DB_HOST || 'localhost';
+    public static DB_USER: string = process.env.DB_USER as string;
+    public static DB_PASSWORD: string = process.env.DB_PASSWORD as string;
+    public static DB_DATABASE: string = process.env.DB_DATABASE as string;
+
+}
